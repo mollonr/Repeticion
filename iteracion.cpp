30b7e17fe05498a8b8cf48bc1c543ec4dbea037a
FukushimaMartin <fukushimafederico@hotmail.com>
2017-04-26 17:31:54 -0300

    Create iteracion.cpp

---
+++ iteracion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+/* 
+* TP 3: "Enviar una frase a la salida estándar muchas veces"
+* Federico Martin Fukushima
+* 26-04-2017
+*/
+
+int main(){
+
+  std::string frase;
+  int a;
+  
+  std::cout<<"ingrese la frase a repetir\n";
+  std::getline (std::cin,frase);
+  std::cout<<"ingrese la cantidad de veces a repetir\n";
+  std::cin>>a;
+  
+  for(int i=0; i<a; i++){
+    std::cout<<i+1<<") "<<frase<<"\n";
+  }
+}
